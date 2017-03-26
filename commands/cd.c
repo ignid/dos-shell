7294ad0e5c8f49e7c1687695ca9c62cae5fcf1c4
ignid <wraysburry@protonmail.com>
2017-03-26 19:03:39 +0700

    Update commands/cd.c

--- commands/cd.c
+++ commands/cd.c
@@ -1,11 +1,3 @@
-#ifdef _WIN32
-#elif defined __linux__
-#include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/statvfs.h>
-#endif
-
 void cd_execute(struct Terminal* terminal, struct Command* self, char* arguments) {
 	Terminal_navigate(terminal, arguments);
 }
